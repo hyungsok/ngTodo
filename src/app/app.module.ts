ead77b2bdd4274922fc2223b81834747602a3065
최철환 <hwanyhee@gmail.com>
2018-04-14 11:23:51 +0900

    라우팅 테이블 설계 - 라우팅 테이블 구성 완료 및 테스트

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,14 @@ import { JqueryComponent } from './jquery/jquery.component';
 import { AngularComponent } from './angular/angular.component';
 import {RouterModule, Routes} from '@angular/router';
 
-const route: Routes = [];
+const route: Routes = [
+  {path: '', component: IndexComponent, children: [
+      {path: '', component: HomeComponent},
+      {path: 'jquery', component: JqueryComponent},
+      {path: 'angular', component: AngularComponent},
+    ]}, // 사용자 사이트
+  // {path: 'admin', loadChildren: 'app/admin/admin.module#AdminModule'}, // 관리자 사이트, lazyloading
+];
 
 @NgModule({
   declarations: [
