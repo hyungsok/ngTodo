046763fc9ec80d37b46a3d25e25beacb1f21ec29
최철환 <hwanyhee@gmail.com>
2018-04-14 11:50:44 +0900

    머티리얼 디자인 적용

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HomeComponent } from './home/home.component';
 import { JqueryComponent } from './jquery/jquery.component';
 import { AngularComponent } from './angular/angular.component';
 import {RouterModule, Routes} from '@angular/router';
+import {MatToolbarModule} from '@angular/material';
 
 const route: Routes = [
   {path: '', component: IndexComponent, children: [
@@ -29,6 +30,7 @@ const route: Routes = [
   imports: [
     BrowserModule,
     RouterModule.forRoot(route),
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
