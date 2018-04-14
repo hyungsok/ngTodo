3a7819c036dcb6161beb59c7a82874bc65b6426c
최철환 <hwanyhee@gmail.com>
2018-04-14 17:12:47 +0900

    development 와 production 의 서버 환경 설정

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,14 +1,24 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
 import {TodoVO} from './domain/todo.vo';
+import {environment} from '../environments/environment';
 
 @Injectable()
 export class UserService {
 
-  constructor(private http: HttpClient) { }
+  private SERVER: string;
+  private headers: HttpHeaders;
+
+  constructor(private http: HttpClient) {
+    this.SERVER = `${environment.HOST}`;
+    this.headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+  }
+
 
   getTodoList(): Observable<TodoVO[]> {
-    return this.http.get<TodoVO[]>('http://www.javabrain.kr:8080/api/todo');
+    return this.http.get<TodoVO[]>(this.SERVER + '/api/todo');
   }
 }
