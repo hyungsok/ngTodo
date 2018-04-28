3aeb3381a8f6caf3cfb843da7b49e00dba63cf05
최철환 <hwanyhee@gmail.com>
2018-04-28 10:12:15 +0900

    html5방식의 validation 처리
    formfield, mat-input, mat-hint를 이용하여 머티리얼 디자인
    form.valid 속성을 이용하여 validation 처리
    => template-driven 폼에서는 폼에는 ngForm input에는 ngModel을
    반듯이 붙여야 한다는 규칙이 있다.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,10 @@ import { HomeComponent } from './home/home.component';
 import { JqueryComponent } from './jquery/jquery.component';
 import { AngularComponent } from './angular/angular.component';
 import {RouterModule, Routes} from '@angular/router';
-import {MatButtonModule, MatCardModule, MatIconModule, MatMenuModule, MatToolbarModule} from '@angular/material';
+import {
+  MatButtonModule, MatCardModule, MatFormFieldModule, MatIconModule, MatInputModule, MatMenuModule,
+  MatToolbarModule
+} from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {UserService} from './user.service';
@@ -44,6 +47,8 @@ const route: Routes = [
     MatIconModule,
     MatButtonModule,
     MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [UserService],
   bootstrap: [AppComponent]
