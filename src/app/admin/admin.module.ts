7191827ea5f4254ffd3395e7f7e92e52dbf95d1d
최철환 <hwanyhee@gmail.com>
2018-04-28 18:08:52 +0900

    관리자 - 뉴스 목록 가져오기 1

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,8 +4,9 @@ import { IndexComponent } from './index/index.component';
 import { HomeComponent } from './home/home.component';
 import { NewsComponent } from './news/news.component';
 import {RouterModule, Routes} from '@angular/router';
-import {MatExpansionModule, MatToolbarModule} from '@angular/material';
+import {MatCardModule, MatExpansionModule, MatToolbarModule} from '@angular/material';
 import {FlexLayoutModule} from '@angular/flex-layout';
+import {AdminService} from './admin.service';
 
 const route: Routes = [
   {path: '', component: IndexComponent, children: [
@@ -21,7 +22,9 @@ const route: Routes = [
     FlexLayoutModule,
     MatToolbarModule,
     MatExpansionModule,
+    MatCardModule,
   ],
-  declarations: [IndexComponent, HomeComponent, NewsComponent]
+  declarations: [IndexComponent, HomeComponent, NewsComponent],
+  providers: [AdminService],
 })
 export class AdminModule { }
--- src/app/admin/news/news.component.ts
+++ src/app/admin/news/news.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AdminService} from '../admin.service';
+import {NewsVO} from '../../domain/news.vo';
 
 @Component({
   selector: 'app-news',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./news.component.scss']
 })
 export class NewsComponent implements OnInit {
+  newsList: NewsVO[];
 
-  constructor() { }
+  constructor(private adminService: AdminService) { }
 
   ngOnInit() {
+    const params = {
+      start_index: 0,
+      page_size: 5
+    };
+
+    this.adminService.findNews(params)
+      .subscribe(body => {
+        console.log(body);
+        // body의 data 필드를 newsList에 담기
+        this.newsList = body.data;
+        console.log(this.newsList);
+      });
   }
 
 }
